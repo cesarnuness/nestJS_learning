bbb22a2d2e148f0885b4e293512a414687d949e2
César Nunes <cesar.nunes@live.com>
2023-09-12 18:53:16 -0300

    initial commit

---
+++ src/user/user.controller.ts
@@ -0,0 +1,42 @@
+import {
+  Body,
+  Controller,
+  Param,
+  Post,
+  Get,
+  Put,
+  Delete,
+  Patch,
+} from '@nestjs/common';
+
+@Controller('users')
+export class UserController {
+  @Post()
+  async create(@Body() body) {
+    return { body };
+  }
+  @Get()
+  async read() {
+    return 'All Users';
+  }
+
+  @Get(':id')
+  async show(@Param() params) {
+    return { params };
+  }
+
+  @Put(':id')
+  async update(@Body() body, @Param() param) {
+    return { body, param };
+  }
+
+  @Patch(':id')
+  async updatePartial(@Body() body, @Param() param) {
+    return { body, param };
+  }
+
+  @Delete(':id')
+  async delete(@Param() param) {
+    return { param };
+  }
+}
